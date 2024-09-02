89b2add3b00ecea28d6f7b077f237a053d45d17f
Alexis <oleksii.tomin@gmail.com>
2024-09-02 16:10:09 -0600

    Add toolbar

--- app/src/main/java/com/oleksii/tomin/portfoliolayouts/BaseActivity.kt
+++ app/src/main/java/com/oleksii/tomin/portfoliolayouts/BaseActivity.kt
@@ -2,12 +2,14 @@ package com.oleksii.tomin.portfoliolayouts
 
 import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
+import androidx.core.content.ContextCompat
 import com.oleksii.tomin.portfoliolayouts.mvi.MviActivity
 
 abstract class BaseActivity : MviActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimary)
 
         onBackPressedDispatcher.addCallback(this, onBackPressedCallback())
     }
